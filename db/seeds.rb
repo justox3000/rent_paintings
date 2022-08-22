38fb1d0219b20fb2f746ae9c7722ff6b4cf46b2a
Justus Lambrecht <justuslambrecht@icloud.com>
2022-08-22 17:18:42 +0200

    added relations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating 5 random paintings and users"
+
+user1 = User.new(
+  Faker::Internet.user('username', 'email', 'password')
+)
+user1.save!
+
+painting1 = Painting.new(
+  name: Faker::TvShows::RuPaul.quote,
+  artist_name: Faker::Artist.name,
+  year: rand(1765..2022),
+  price: rand(10..666)
+)
+p painting1
+painting1.user_id = user1.id
+p painting1.valid?
+painting1.save!
