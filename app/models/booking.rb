312a96605c1c084e01aa24b07b8b585a37d2d12d
Eri Fujimoto <p32060125@yahoo.co.jp>
2022-08-22 14:35:43 +0100

    added validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user_id
   belongs_to :painting_id
   belongs_to :review_id
+  validates :startdate, :enddate, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  validates :content, length: { minimum: 20 }
+  validates :rating, numericality: { in: 0..5 }
 end
