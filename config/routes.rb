c380783fbfb0f0d355d153583898ce204317422b
Eri Fujimoto <p32060125@yahoo.co.jp>
2022-08-24 10:42:52 +0100

    worked on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "paintings#index"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
